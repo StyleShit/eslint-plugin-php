7959c7481d249ef960f866cb450c458fa857f670
StyleShit <32631382+StyleShit@users.noreply.github.com>
2025-04-30 09:51:44 +0300

    wip

--- src/configs/recommended.ts
+++ src/configs/recommended.ts
@@ -1,6 +1,7 @@
 import { type Linter } from 'eslint';
 
 export const recommended: Linter.Config = {
+	language: 'php/php',
 	rules: {
 		'php/eqeqeq': 'error',
 		'php/disallow-references': 'error',
